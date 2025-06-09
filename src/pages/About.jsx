6cccb2fc6d5e68404e76ec444062fa759ea8dc09
solankiutkrisht <solanki.utkrisht@gmail.com>
2025-06-09 17:20:30 +0530

    chore:updated-stater-code

---
+++ src/pages/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+      <h1>About the Task Manager</h1>
+      <p>This is a simple task management application built with React.</p>
+    </div>
+  )
+}
+
+export default About
